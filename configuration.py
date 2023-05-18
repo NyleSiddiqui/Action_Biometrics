4e9912cf43afe60924aa13ed2d9985a02e8220f7
Nyle Siddiqui <lewelfez98@gmail.com>
2023-05-18 18:06:19 -0400

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -4,8 +4,8 @@ def build_config(dataset):
     cfg = type('', (), {})()
     if dataset == 'ntu_rgbd_120':
         cfg.videos_folder =  '/home/c3-0/datasets/NTU_RGBD_120/nturgb+d_rgb'
-        cfg.train_annotations = '/home/siddiqui/Action_Biometrics/data/NTUTrain_CVmap.csv'
-        cfg.test_annotations = '/home/siddiqui/Action_Biometrics/data/NTUTest_CVmap.csv'
+        cfg.train_annotations = '/home/siddiqui/Action_Biometrics-RGB/data/NTUTrain_CVmap.csv'
+        cfg.test_annotations = '/home/siddiqui/Action_Biometrics-RGB/data/NTUTest_CVmap.csv'
         cfg.train_subjects = range(80) #CS: 53, CV: 80
         cfg.test_subjects = range(69)  #CS: 53, CV: 69
         cfg.num_actions = 120
@@ -23,8 +23,8 @@ def build_config(dataset):
     elif dataset == "pkummd":
         cfg.videos_folder = 'C://Users/ny525072/Downloads//RGB_VIDEO'
         cfg.skeleton_folder = 'C://Users/ny525072/Downloads//skeleton_ntu'
-        cfg.train_annotations = 'C://Users/ny525072/PycharmProjects/Multiview_Actions/PKUMMDTrainCS_map.csv'
-        cfg.test_annotations = 'C://Users/ny525072/PycharmProjects/Multiview_Actions/PKUMMDTestCS_map.csv'
+        cfg.train_annotations = '/home/siddiqui/Action_Biometrics-RGB/data/PKUMMDTrainCS_map.csv'
+        cfg.test_annotations = '/home/siddiqui/Action_Biometrics-RGB/data/PKUMMDTestCS_map.csv'
         cfg.train_subjects = range(56) #CS: 56 CV: 66
         cfg.test_subjects = range(7)  #CS:  7 CV: 63
         cfg.num_subjects = 66

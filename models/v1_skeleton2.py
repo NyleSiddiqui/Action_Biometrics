e04d46610e24f2767297d02b1908f55785967cbc
Nyle Siddiqui <lewelfez98@gmail.com>
2023-05-18 15:32:02 -0400

    Add files via upload

--- models/v1_skeleton2.py
+++ models/v1_skeleton2.py
@@ -5,11 +5,11 @@ from einops import repeat
 
 
 class VideoTransformer(nn.Module):
-    def __init__(self, input_size, num_frames, num_views, num_actions, patch_size, hidden_dim, num_heads, num_layers):
+    def __init__(self, input_size, num_frames, num_subjects, num_actions, patch_size, hidden_dim, num_heads, num_layers):
         super(VideoTransformer, self).__init__()
         self.input_size = input_size
         self.num_frames = num_frames
-        self.num_views = num_views
+        self.num_subjects = num_subjects
         self.num_actions = num_actions
         self.num_layers = num_layers
         self.patch_size = patch_size
@@ -24,18 +24,20 @@ class VideoTransformer(nn.Module):
         self.positional_encoding = nn.Parameter(torch.zeros(self.patch_size, self.backbone_output), requires_grad=True) 
         
         self.encoder = nn.ModuleList([nn.TransformerEncoderLayer(self.backbone_output, num_heads, dim_feedforward=hidden_dim, activation='gelu', batch_first=True, dropout=0.0, norm_first=False) for i in range(num_layers)])
-                    
-        self.transformer_decoder_layer = nn.TransformerDecoderLayer(self.backbone_output, num_heads, dim_feedforward=hidden_dim, activation='gelu', batch_first=True, dropout=0.0, norm_first=False)
-        self.tranformer_decoder = nn.TransformerDecoder(self.transformer_decoder_layer, num_layers)
-        
-        self.action_tokens =  nn.Parameter(torch.unsqueeze(get_orthogonal_queries(20, self.backbone_output), dim=0))
-        #self.view_tokens = nn.Parameter(torch.unsqueeze(get_orthogonal_queries(1, self.backbone_output), dim=0))
-        self.view_tokens = nn.Parameter(torch.randn(1, 1, self.backbone_output))
-        
-        self.view_weights = nn.Parameter(torch.ones(self.num_views))
-        self.action_weights = nn.Parameter(torch.ones(num_actions))
-        
-        self.mlp_head_view = nn.Linear(self.backbone_output, self.num_views)
+
+        self.subject_decoder_l = nn.TransformerDecoderLayer(self.backbone_output, num_heads, dim_feedforward=hidden_dim,
+                                                            activation='gelu', batch_first=True, dropout=0.0,
+                                                            norm_first=False)
+        self.action_decoder_l = nn.TransformerDecoderLayer(self.backbone_output, num_heads, dim_feedforward=hidden_dim,
+                                                           activation='gelu', batch_first=True, dropout=0.0,
+                                                           norm_first=False)
+        self.subject_decoder = nn.TransformerDecoder(self.subject_decoder_l, self.num_layers)
+        self.action_decoder = nn.TransformerDecoder(self.action_decoder_l, self.num_layers)
+
+        self.action_tokens = nn.Parameter(torch.unsqueeze(get_orthogonal_queries(20, self.backbone_output), dim=0))
+        self.subject_tokens = nn.Parameter(torch.unsqueeze(get_orthogonal_queries(20, self.backbone_output), dim=0))
+
+        self.mlp_head_subject = nn.Linear(self.backbone_output, num_subjects)
         self.mlp_head_action = nn.Linear(self.backbone_output, num_actions)
         
 
@@ -47,30 +49,20 @@ class VideoTransformer(nn.Module):
     
         features += self.positional_encoding[None, :features.shape[1], :]
         #print(features.shape)
-        
+
         action_tokens = repeat(self.action_tokens, '() n d -> b n d', b=bs)
-        view_tokens = repeat(self.view_tokens, '() n d -> b n d', b=bs)
-        #print(action_tokens.shape, view_tokens.shape)
-        
-        queries = torch.cat((action_tokens, view_tokens), dim=1) # b x 16 x f
-        #print(queries.shape)
-        out = self.tranformer_decoder(queries, features)
-        #print(out.shape)
-        x_act, x_view = out[:, :-1, :], out[:, -1, :]
-        #print(x_act.shape, x_view.shape)
-        
-        
-        #features_view = torch.mean(features_view * self.view_weights[None, :, None], dim=1) # b x queries x f
-        #features_action = torch.mean(features_action * self.action_weights[None, :, None], dim=1)
-        
-        features_view = x_view
-        features_action = x_act.mean(dim=1)
-        
-        output_views = self.mlp_head_view(features_view)
+        subject_tokens = repeat(self.subject_tokens, '() n d -> b n d', b=bs)
+
+        features_subject = self.subject_decoder(subject_tokens, features)
+        features_action = self.action_decoder(action_tokens, features)
+
+        features_subject = features_subject.mean(dim=1)
+        features_action = features_action.mean(dim=1)
+
+        output_subjects = self.mlp_head_subject(features_subject)
         output_actions = self.mlp_head_action(features_action)
-        
-        
-        return output_views, output_actions, features_view, features_action, view_tokens, action_tokens
+
+        return output_subjects, output_actions, features_subject, features_action, subject_tokens, action_tokens
         
         
 def generate_orthogonal_vectors(N,d):
